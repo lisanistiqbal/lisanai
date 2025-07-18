d16e1ecfa21625c17fb48ac604e93070cbc52c40
lisanistiqbal <iqbalasifpat@gmail.com>
2025-07-18 18:38:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -691,7 +691,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-def df_to_json(df, source_col = 'Source', target_col= 'Target'):
+def df_to_json(df, source_col = 'source', target_col= 'target'):
     return df[[source_col]].rename(columns={source_col: 'text'}).to_dict(orient='records')
 def df_to_json_single_quotes(df, source_col='Source', target_col='Target'):
     # Convert to JSON string with proper escaping
