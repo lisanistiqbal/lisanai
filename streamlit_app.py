4393f361ac4a15d9e24770c0d8525a73c989718d
lisanistiqbal <iqbalasifpat@gmail.com>
2025-07-30 17:20:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -759,11 +759,11 @@ def translate_json(text_json, target, source, tone, domain, instruction, mandato
     print(raw)
     return eval(raw)
 
-def batch_translate_df(df, source, target, tone, domain, instruction, mandatory_translations, source_col, target_col, batch_size=40, delay=1):
+def batch_translate_df(df, source, target, tone, domain, instruction, mandatory_translations, source_col, target_col, batch_size=40, delay=2):
     result_df = pd.DataFrame()
     for i in range(0, len(df), batch_size):
         batch = df.iloc[i:i+batch_size]
-        json_payload = df_to_json_single_quotes(df, source_col, target_col)
+        json_payload = df_to_json_single_quotes(batch, source_col, target_col)
         translated_json = translate_json(json_payload, target, source, tone, domain, instruction, mandatory_translations, source_col, target_col)
         batch_result = pd.DataFrame(translated_json)
         result_df = pd.concat([result_df, batch_result], ignore_index=True)
@@ -1161,10 +1161,8 @@ else:
                 st.dataframe(df)
 
             elif file_extension == "xlsx":
-                df = pd.read_excel(uploaded_file)  # Load all sheets
-                st.dataframe(df)
-                json_payload = df_to_json_single_quotes(df, source_col, target_col)
-
+                df_excel = pd.read_excel(uploaded_file)  # Load all sheets
+                st.dataframe(df_excel)
             elif file_extension == "pdf":
                 # Read PDF file
                 pdf_reader = PyPDF2.PdfReader(uploaded_file)
@@ -1200,10 +1198,11 @@ else:
                     st.download_button("Download Translated File", data=translated_text, file_name=f"Translated_{filename}")
                 elif file_extension == "xlsx":
                     if len(df) <= 75:
+                        json_payload = df_to_json_single_quotes(df_excel, source_col, target_col)
                         translated_json = translate_json(json_payload, target, source, tone, domain, instruction, mandatory_translations, source_col, target_col)
                         df_res = pd.DataFrame(translated_json)
                     else:
-                        df_res = batch_translate_df(df, source, target, tone, domain, instruction, mandatory_translations, source_col, target_col)
+                        df_res = batch_translate_df(df_excel, source, target, tone, domain, instruction, mandatory_translations, source_col, target_col)
                     #df_res.rename(columns={'translated': target_col}, inplace=True)
                     st.dataframe(df_res)
                     
